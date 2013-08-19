a2d56f51f7061419b4447af41f6c8819d60df33e
Ubuntu <ubuntu@ip-10-202-193-50.ec2.internal>
2013-08-19 02:36:06 +0000

    Python version.

---
+++ ex-1.12.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+'''
+Exercise: Create a recursive procedure for calculating Pascal's Triangle.
+
+I was able to create the function to calculate the value of a single cell, given its
+line and cell number, but creating the recursive call to concatenate these values into
+a list was proving tricky in Scheme. Here is an implementation of the procedure in Python.
+
+For fun, the third function will print the entire triangle given the number of rows.
+Not sure if there is a more idiomatic way of pulling off those ranges, since their values
+are integers to be calculated and not indexes.
+'''
+import sys
+
+def calculate_one(line, place):
+    if place == 1 or line == place:
+        return 1
+    else:
+        return calculate_one(line - 1, place - 1) + calculate_one(line - 1, place)
+
+def write_line(line):
+    return [calculate_one(line, i) for i in range(1, line + 1)]
+
+def write_triangle(lines):
+    for i in range(1, lines +1):
+        print write_line(i)
+
+if __name__ == '__main__':
+    write_triangle(int(sys.argv[1]))
